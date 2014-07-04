fd067bc993c5eef9632f1b5e45353ce52ddde7e4
Gergely Nagy <gnanet@users.noreply.github.com>
2014-07-04 23:56:54 +0200

    Initial version of oc-generate-share-keys.php
    The script generates a new set of shareKeys and the encrypted file-key for an encrypted shared file if the share owner already had its shareKey.
    
    It is based on the example script at the blog of @schiesbn:
    http://blog.schiessle.org/2013/05/28/introduction-to-the-new-owncloud-encryption-app/comment-page-1/#comment-61180

---
+++ oc-generate-share-keys.php
@@ -0,0 +1,63 @@
+<?php
+
+
+// This script is meant to be called from CLI
+
+// The script generates a new set of shareKeys and the encrypted file-key for an encrypted shared file
+// if the share owner already has its
+
+
+if ($argc<2) {
+fwrite(STDERR, "Object needed\n");
+exit(1);
+}
+
+
+// I placed the script in a subfolder of owncloud web-root
+require_once '../apps/files_encryption/lib/crypt.php';
+
+$SHAREOWNER = 'masteruser';
+$OWNERPASSWORD = 'somepassword';
+$myAllowUsers = array($SHAREOWNER,"user1", "user2", "user3", "user4");
+
+// the file for we need new keys has to be relative to the "files" folder of the share-owner
+$FILEFULLNAME = $argv[1];
+$FILENAME = basename($FILEFULLNAME);
+
+$OCDATADIR="/path-to-oc-datadir-no-trailing-slash";
+
+
+// first get share owners private key and decrypt it
+$encryptedUserKey = file_get_contents("$OCDATADIR/$SHAREOWNER/files_encryption/$SHAREOWNER.private.key");
+$decryptedUserKey = OCA\Encryption\Crypt::decryptPrivateKey($encryptedUserKey, $OWNERPASSWORD);
+
+// now we need to decrypt the file-key, therefore we use the private key and the share key
+$shareKey = file_get_contents("$OCDATADIR/$SHAREOWNER/files_encryption/share-keys/$FILEFULLNAME.$SHAREOWNER.shareKey");
+$encryptedKeyfile = file_get_contents("$OCDATADIR/$SHAREOWNER/files_encryption/keyfiles/$FILEFULLNAME.key");
+$decryptedKeyfile = OCA\Encryption\Crypt::multiKeyDecrypt($encryptedKeyfile, $shareKey, $decryptedUserKey);
+
+// then we get the users public key
+$userPubKeys = array();
+foreach ( $myAllowUsers as $myAllowUser ) {
+        $userPubKeys[$myAllowUser] = file_get_contents("$OCDATADIR/public-keys/" . $myAllowUser . '.public.key');
+}
+
+// generating the new keys
+$multiEncKey = OCA\Encryption\Crypt::multiKeyEncrypt($decryptedKeyfile, $userPubKeys);
+
+$newshareKeys = $multiEncKey['keys'];
+$newKeyfile = $multiEncKey['data'];
+
+
+// TODO the recursive folder structure of ./work has to be created to place the files in a simply copiable stucture
+//
+
+// storing the key files
+foreach ($newshareKeys as $userId => $newshareKey) {
+        file_put_contents("./work/share/". $FILEFULLNAME . "." . $userId . ".shareKey", $newshareKey);
+}
+
+file_put_contents("./work/key/" . $FILEFULLNAME . ".key", $newKeyfile);
+
+fwrite(STDERR, "Done $FILENAME\n");
+
